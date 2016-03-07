4220320459ffad37c2c19115af83a56043634db8
mydongistiny <jaysonedson@gmail.com>
2016-03-06 16:02:12 -0800

    Powermenu Torch option [2/2]
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/maple/PowerMenuActions.java
+++ src/com/android/settings/maple/PowerMenuActions.java
@@ -50,6 +50,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private SwitchPreference mSettingsPref;
     private SwitchPreference mLockdownPref;
     private SwitchPreference mBugReportPref;
+    private SwitchPreference mFlashlightPref;
     private SwitchPreference mSilentPref;
 
     Context mContext;
@@ -93,6 +94,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mLockdownPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
             } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
                 mBugReportPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_TORCH)) {
+                mFlashlightPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_TORCH);
             } else if (action.equals(GLOBAL_ACTION_KEY_SILENT)) {
                 mSilentPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SILENT);
             }
@@ -154,6 +157,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             mBugReportPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_BUGREPORT));
         }
 
+        if (mScreenrecordPref != null) {
+            mFlashlightPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_TORCH));
+        }
+
         if (mSilentPref != null) {
             mSilentPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SILENT));
         }
@@ -206,6 +213,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         } else if (preference == mBugReportPref) {
             value = mBugReportPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
+
+        } else if (preference == mFlashlightPref) {
+            value = mFlashlightPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_TORCH);
 
         } else if (preference == mSilentPref) {
             value = mSilentPref.isChecked();
